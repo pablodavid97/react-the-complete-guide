dfe33c48d054d72019388b246d82dd68016d136b
Pablo Llanes <pllanesparamo@gmail.com>
2024-10-28 13:28:12 -0500

    Start section 12 practice project

---
+++ section12/react-quiz-project/src/components/Quiz.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+import QUESTIONS from '../questions.js';
+import QuestionTimer from './QuestionTimer.jsx';
+import quizCompleteImg from '../assets/quiz-complete.png';
+
+const Quiz = () => {
+    const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+    const handleSelectAnswer = (selectedAnswer) => {
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer];
+        });
+    };
+
+    if (quizIsComplete) {
+        return (
+            <div id='summary'>
+                <img src={quizCompleteImg} alt='Trophy icon' />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
+    return (
+        <div id='quiz'>
+            <div id='question'>
+                <QuestionTimer
+                    timeout={10000}
+                    onTimeout={() => handleSelectAnswer(null)}
+                />
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>{' '}
+                <ul id='answers'>
+                    {shuffledAnswers.map((answer) => (
+                        <li key={answer} className='answer'>
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Quiz;
