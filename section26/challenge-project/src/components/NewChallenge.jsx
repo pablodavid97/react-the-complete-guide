bfb8291d2867f3354b58b36d2d1f804c9bd8454b
Pablo Llanes <pllanesparamo@gmail.com>
2024-11-25 15:41:34 -0500

    Finish section 26 - react animations with framer motion

--- section26/challenge-project/src/components/NewChallenge.jsx
+++ section26/challenge-project/src/components/NewChallenge.jsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, useAnimate, stagger } from 'framer-motion';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import Modal from './Modal.jsx';
@@ -9,6 +9,8 @@ export default function NewChallenge({ onDone }) {
     const title = useRef();
     const description = useRef();
     const deadline = useRef();
+
+    const [scope, animate] = useAnimate();
 
     const [selectedImage, setSelectedImage] = useState(null);
     const { addChallenge } = useContext(ChallengesContext);
@@ -32,6 +34,11 @@ export default function NewChallenge({ onDone }) {
             !challenge.deadline.trim() ||
             !challenge.image
         ) {
+            animate(
+                'input, textarea',
+                { x: [-10, 0, 10, 0] },
+                { type: 'spring', duration: 0.2, delay: stagger(0.05) }
+            );
             return;
         }
 
@@ -41,7 +48,7 @@ export default function NewChallenge({ onDone }) {
 
     return (
         <Modal title='New Challenge' onClose={onDone}>
-            <form id='new-challenge' onSubmit={handleSubmit}>
+            <form ref={scope} id='new-challenge' onSubmit={handleSubmit}>
                 <p>
                     <label htmlFor='title'>Title</label>
                     <input ref={title} type='text' name='title' id='title' />
@@ -66,12 +73,17 @@ export default function NewChallenge({ onDone }) {
                     />
                 </p>
 
-                <ul id='new-challenge-images'>
+                <motion.ul
+                    id='new-challenge-images'
+                    variants={{
+                        visible: { transition: { staggerChildren: 0.05 } },
+                    }}
+                >
                     {images.map((image) => (
                         <motion.li
                             variants={{
-                                hidden: { opacity: 0, scale: 0.3 },
-                                visible: { opacity: 1, scale: 1 },
+                                hidden: { opacity: 0, scale: 0.5 },
+                                visible: { opacity: 1, scale: [0.8, 1.3, 1] },
                             }}
                             transition={{ type: 'spring' }}
                             key={image.alt}
@@ -83,7 +95,7 @@ export default function NewChallenge({ onDone }) {
                             <img {...image} />
                         </motion.li>
                     ))}
-                </ul>
+                </motion.ul>
 
                 <p className='new-challenge-actions'>
                     <button type='button' onClick={onDone}>
