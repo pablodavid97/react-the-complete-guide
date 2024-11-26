6bd317b6f0ed62b6dde879a48a58d85816a1b909
Pablo Llanes <pllanesparamo@gmail.com>
2024-11-26 15:54:47 -0500

    Finish section 35 - Components practice

---
+++ section35/react-concepts-app/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Concepts from './components/Concepts';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Concepts />
+        </>
+    );
+}
+
+export default App;
