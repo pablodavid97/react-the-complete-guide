c014c3c1153b1531b04461bb79189036a28d3413
Pablo Llanes <pllanesparamo@gmail.com>
2024-11-27 17:23:18 -0500

    Start section 31

---
+++ section31/react-crash-course/src/components/Modal.jsx
@@ -0,0 +1,14 @@
+import styles from './Modal.module.css';
+
+const Modal = ({ children, onClose }) => {
+    return (
+        <>
+            <div className={styles.backdrop} onClick={onClose}></div>
+            <dialog open className={styles.modal}>
+                {children}
+            </dialog>
+        </>
+    );
+};
+
+export default Modal;
