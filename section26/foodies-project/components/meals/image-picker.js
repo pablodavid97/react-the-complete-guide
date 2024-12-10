02e01f75a8e4a6acf136ccbdff4841857b9d8392
Pablo Llanes <pllanesparamo@gmail.com>
2024-12-10 15:01:58 -0500

    Added meals DB and backend validations

---
+++ section26/foodies-project/components/meals/image-picker.js
@@ -0,0 +1,68 @@
+'use client';
+
+import { useState, useRef } from 'react';
+import Image from 'next/image';
+
+import styles from './image-picker.module.css';
+
+const ImagePicker = ({ label, name }) => {
+    const [pickedImage, setPickedImage] = useState();
+    const imageInputRef = useRef();
+
+    const handleImagePickerClick = () => {
+        imageInputRef.current.click();
+    };
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+    };
+
+    return (
+        <div className={styles.picker}>
+            <label htmlFor={name}>{label}</label>
+            <div className={styles.controls}>
+                <div className={styles.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && (
+                        <Image
+                            src={pickedImage}
+                            alt='The image selected by user.'
+                            fill
+                        />
+                    )}
+                </div>
+                <input
+                    className={styles.input}
+                    type='file'
+                    id={name}
+                    accept='image/png, image/jpef'
+                    name={name}
+                    ref={imageInputRef}
+                    onChange={handleImageChange}
+                    required
+                />
+                <button
+                    className={styles.button}
+                    type='button'
+                    onClick={handleImagePickerClick}
+                >
+                    Pick an Image
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ImagePicker;
